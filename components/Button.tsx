2e74c5f1a52d837c3b6eefc9ac0bd90d5dad221f
Douglas Lowder <douglowder@mac.com>
2025-04-10 16:16:56 -0700

    Fix button usability

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,4 @@
-import { BorderlessButton } from 'react-native-gesture-handler';
+import { Pressable } from 'react-native-gesture-handler';
 import { FontSize, Margin, TailwindColor } from '@/constants/styles';
 
 import { ThemedText as Text } from './ThemedText';
@@ -13,28 +13,32 @@ export default function Button({
   disabled?: boolean;
 }) {
   return (
-    <BorderlessButton
-      borderless={false}
+    <Pressable
       hitSlop={{ top: 16, left: 16, bottom: 16, right: 16 }}
-      enabled={!disabled}
+      disabled={disabled}
       onPress={onPress}
     >
-      <Text
-        lightColor={
-          disabled ? TailwindColor['gray-500'] : TailwindColor['blue-500']
-        }
-        darkColor={
-          disabled ? TailwindColor['gray-500'] : TailwindColor['white']
-        }
-        style={{
-          fontSize: FontSize.xl,
-          marginTop: Margin[1],
-          marginBottom: Margin[4],
-          textAlign: 'center',
-        }}
-      >
-        {title}
-      </Text>
-    </BorderlessButton>
+      {({ pressed }) => {
+        return (
+          <Text
+            lightColor={
+              disabled ? TailwindColor['gray-500'] : TailwindColor['blue-500']
+            }
+            darkColor={
+              disabled ? TailwindColor['gray-500'] : TailwindColor['white']
+            }
+            style={{
+              fontSize: FontSize.xl,
+              marginTop: Margin[1],
+              marginBottom: Margin[4],
+              textAlign: 'center',
+              opacity: pressed ? 0.5 : 1,
+            }}
+          >
+            {title}
+          </Text>
+        );
+      }}
+    </Pressable>
   );
 }
